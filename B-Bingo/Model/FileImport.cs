2c330b0d11c1ee0d3de816783a87cc2fd05afd55
Christian Scheel <christian.scheel@bluewin.ch>
2021-06-08 15:34:59 +0200

    Enable basic file file import
    Number of Strings, path of file and so on is hardcoded for the beginning.
    It will be replaced later with a better method and added
    exception handling.

--- B-Bingo/Model/FileImport.cs
+++ B-Bingo/Model/FileImport.cs
@@ -1,10 +1,24 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
+
 
 namespace B_Bingo.Model
 {
     class FileImport
     {
+        //everything is hardcoded for the beginning. will be replaced later
+        private string buzzWordsTogether = string.Empty;
+        private string[] buzzWordsSeparated = new string[16];
+
+        public string[] ImportFileData()
+        {
+            //TODO: add exception handling
+            buzzWordsTogether = File.ReadAllText(@"C:\Users\Chris\Documents\Programmieren\B-Bingo\B-Words.txt");
+            buzzWordsSeparated = buzzWordsTogether.Split(", ");
+            return buzzWordsSeparated;
+        }
     }
 }
